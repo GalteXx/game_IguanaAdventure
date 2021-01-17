239ac5d9d238cf85fa3259935755be84be21cb87
taggeddev <denis.mikov.06@gmail.com>
2021-01-17 15:02:31 +0300

    First movement, textures, tilemap cuts
    First movement, textures, tilemap cuts

---
+++ Assets/Scripts/Lizard.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+    public bool isGrounded = false;
+    public Vector2 force = new Vector2(0f, 5f);
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded == true) { 
+            gameObject.GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
+        }
+    }
+}
