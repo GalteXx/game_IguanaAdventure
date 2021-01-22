039289c91e0517cf8b79a8cf7879329dcef39a77
taggeddev <denis.mikov.06@gmail.com>
2021-01-22 21:18:11 +0300

    Smooth camera follow
    Smooth camera follow

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform target;
+
+    public float speed = 0.125f;
+    public Vector3 offset;
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, speed);
+        transform.position = smoothedPosition;
+    }
+}
