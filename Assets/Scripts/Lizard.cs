e50bddb8adebecd613b884896523a12496afd753
taggeddev <denis.mikov.06@gmail.com>
2021-01-28 21:07:37 +0300

    Made edits in Character funcs

--- Assets/Scripts/Lizard.cs
+++ Assets/Scripts/Lizard.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Lizard : MonoBehaviour
 {
-    // Start is called before the first frame update
     public Animator anim;
+    public Vector2 force = new Vector2(0f, 5f);
+
     public float moveSpeed = 5f;
+
     public bool isGrounded = false;
     public bool facingRight = true;
-    public Vector2 force = new Vector2(0f, 5f);
     private bool attack;
 
     void Start()
@@ -17,27 +18,35 @@ public class Lizard : MonoBehaviour
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Jump();
-
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * moveSpeed;
+
+        // Jump on spacebar
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            Jump();
+        }
+       
+        // Set animation to walking if x axis has difference >0 or its not walking, then turn off walking animation
         if (movement.x > 0 || movement.x < 0)
         {
             anim.SetBool("isRunning", true);
-        } else
+        } 
+        else 
         {
             anim.SetBool("isRunning", false);
         }
         
+        // Attack on LeftShift but can be changed. In work
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             attack = true;
             Attack();
         }
 
+        // Flip if changed direction
         if ( (movement.x < 0 && facingRight) || (movement.x > 0 && !facingRight))
         {
             Flip();
@@ -57,9 +66,7 @@ public class Lizard : MonoBehaviour
 
     void Jump()
     {
-        if (Input.GetButtonDown("Jump") && isGrounded == true) { 
-            gameObject.GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
-        }
+        gameObject.GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
     }
 
     void Flip()
