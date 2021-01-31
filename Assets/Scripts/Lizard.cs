d05403b19e70c39ee51a3e2404cb0370f78cb90f
taggeddev <denis.mikov.06@gmail.com>
2021-01-31 18:08:15 +0300

    Found bug, edited jump, added timer
    Bug column created on trello board

--- Assets/Scripts/Lizard.cs
+++ Assets/Scripts/Lizard.cs
@@ -6,35 +6,35 @@ public class Lizard : MonoBehaviour
 {
     public Animator anim;
     public Vector2 force = new Vector2(0f, 5f);
+    public Rigidbody2D rb;
 
     public float moveSpeed = 5f;
+    public float hangTime = 2f;
+    private float hangCounter = 0f;
 
     public bool isGrounded = false;
     public bool facingRight = true;
-
-    void Start()
-    {
-        
-    }
-
+    
     void Update()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement * Time.deltaTime * moveSpeed;
+        // Checking the side player is going, left or right
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f); // so its either -1 or 1
+        transform.position += movement * Time.deltaTime * moveSpeed; // so the position of our player is -1 or 1 * time has passed and ms 
+        
+        // ***=== Jumping section start ===*** \\
+        HangManager(); // After player leaves platform timer
 
-        // Jump on spacebar
-        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        if (Input.GetButtonDown("Jump") && hangCounter > 0)
         {
             Jump();
-        }
+        } // Jump on spacebar
+        // ***=== Jumping section end ===*** \\
 
         // Attack on LeftShift but can be changed. In work
         if (Input.GetMouseButtonDown(0))
         {
             Attack();
         }
-
-        //
 
         // Set animation to walking if x axis has difference >0 or its not walking, then turn off walking animation
         if (movement.x > 0 || movement.x < 0)
@@ -54,6 +54,18 @@ public class Lizard : MonoBehaviour
 
     }
 
+    void HangManager()
+    {
+        if (isGrounded)
+        {
+            hangCounter = hangTime;
+        }
+        else
+        {
+            hangCounter -= Time.deltaTime;
+        }
+    }
+
     void Attack()
     {
         anim.SetTrigger("attack");
@@ -62,7 +74,14 @@ public class Lizard : MonoBehaviour
 
     void Jump()
     {
-        gameObject.GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
+        if (Input.GetButtonDown("Jump") && rb.velocity.y > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * .3f);
+        } else if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = force;
+        //gameObject.GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
+        }
     }
 
     void Flip()
