8c565ee702419bdb6c087f2f25e95ed669cf9581
taggeddev <denis.mikov.06@gmail.com>
2021-02-22 17:42:09 +0300

    P(a)ra(nal)lax

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,26 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Transform target;
+    Material mat;
+    Vector2 offset = Vector2.zero;
+
+    [SerializeField] float scale = 1.0f;
+
+    void Start()
+    {
+        target = transform.root;
+        mat = GetComponent<SpriteRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset = new Vector2( target.position.x / 100f / scale, -3f);
+        mat.mainTextureOffset = offset;
+    }
+}
