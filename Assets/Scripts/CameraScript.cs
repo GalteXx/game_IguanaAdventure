4846f4480b08514b2f46830f297d01b69a05fa34
taggeddev <denis.mikov.06@gmail.com>
2021-03-05 18:34:16 +0300

    parallax fixed I am a hostage help me
    aa

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -11,7 +11,8 @@ public class CameraScript : MonoBehaviour
 
     void LateUpdate()
     {
-        Vector3 desiredPosition = target.position + offset;
+        //Vector3 desiredPosition = target.position + offset;
+        Vector3 desiredPosition = new Vector3(target.position.x + offset.x, transform.position.y, -11);
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, speed);
         transform.position = smoothedPosition;
     }
