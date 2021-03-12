8e309d00f4b70cb53e167476b5fb4ab8b2d4e2e7
taggeddev <denis.mikov.06@gmail.com>
2021-03-12 17:43:18 +0300

    Fixed bugs
    1. Coping parallax effect
    2. Errors on Destroy(Object)
    3. Possibility to loop the attack

--- Assets/Scripts/Lizard.cs
+++ Assets/Scripts/Lizard.cs
@@ -5,63 +5,93 @@ using UnityEngine;
 public class Lizard : MonoBehaviour
 {
     public Animator anim;
+    public Transform checkpoint;
     public Vector2 force = new Vector2(0f, 5f);
     public Rigidbody2D rb;
+    //public GameObject go;
     [SerializeField] Transform groundCheckCircle;
     [SerializeField] LayerMask groundLayer;
 
     const float groundCheckRadius = .2f;
 
-    public float moveSpeed = 5f;
-    public float hangTime = 2f;
+    private float moveSpeed = 3.9f;
+    private float hangTime = 2f;
+    public float currentSpeed;
     private float hangCounter = 0f;
 
-    public bool isGrounded = false;
     public bool facingRight = true;
-    
+
+    private bool isGrounded = false;
+    private bool isAttacking = false;
+    private bool isDead = false;
+    private bool isAttackPressed = false;
+
+    private string currentState;
+
+    const string PLAYER_IDLE = "Idle";
+    const string PLAYER_RUN = "Run";
+    const string PLAYER_ATTACK = "Attack";
+
     void Update()
     {
-        // Checking the side player is going, left or right
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f); // so its either -1 or 1
-        transform.position += movement * Time.deltaTime * moveSpeed; // so the position of our player is -1 or 1 * time has passed and ms 
-        GroundCheck();
-        // ***=== Jumping section start ===*** \\
-        HangManager(); // After player leaves platform timer
-
+        // Jump on spacebar
         if (Input.GetButtonDown("Jump") && hangCounter > 0)
         {
             Jump();
-        } // Jump on spacebar
-        // ***=== Jumping section end ===*** \\
+        } 
 
         // Attack on LeftShift but can be changed. In work
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            Attack();
-            anim.SetBool("isAttacking", true);
-        }
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            Attack();
-            anim.SetBool("isAttacking", false);
+        if (!isAttacking && !isDead) { 
+            if (currentSpeed == 0)
+                ChangeAnimation(PLAYER_IDLE);
+            else if (currentSpeed > 0)
+                ChangeAnimation(PLAYER_RUN);
+            
         }
 
-        // Set animation to walking if x axis has difference >0 or its not walking, then turn off walking animation
-        if (movement.x > 0 || movement.x < 0)
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            anim.SetBool("isRunning", true);
-        } 
-        else 
-        {
-            anim.SetBool("isRunning", false);
+            isAttackPressed = true;
+            Attack();
         }
-        
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Respawn();
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        GroundCheck();
+        HangManager();
+
+        // movement
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f); // so its either -1 or 1
+        transform.position += movement * Time.deltaTime * moveSpeed; // so the position of our player is -1 or 1 * time has passed and ms 
+        currentSpeed = Mathf.Abs(movement.x * moveSpeed);
+
         // Flip if changed direction
-        if ( (movement.x < 0 && facingRight) || (movement.x > 0 && !facingRight))
+        if ((movement.x < 0 && facingRight) || (movement.x > 0 && !facingRight))
         {
             Flip();
         }
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Water")
+            Die();
+        
+    }
+
+    void ChangeAnimation(string newState)
+    {
+        if (currentState == newState) return; // prevent animation to interrupt itself
+
+        anim.Play(newState); // play new state
+        currentState = newState; // reassign currentState
     }
 
     void HangManager()
@@ -90,15 +120,29 @@ public class Lizard : MonoBehaviour
 
     void Attack()
     {
-        anim.SetTrigger("attack");
+        if (isAttackPressed)
+        {
+            isAttackPressed = false;
+            if (!isAttacking && !isDead)
+            {
+                isAttacking = true;
+                ChangeAnimation(PLAYER_ATTACK);
+                Invoke("AttackComplete", .2f);
+            }
+        }
+    }
+
+    void AttackComplete()
+    {
+        isAttacking = false;
     }
 
     void Jump()
     {
-        if (Input.GetButtonDown("Jump") && rb.velocity.y > 0)
+        if (Input.GetButtonDown("Jump") && rb.velocity.y > 0 && !isDead)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * .3f);
-        } else if (Input.GetButtonDown("Jump"))
+        } else if (Input.GetButtonDown("Jump") && !isDead)
         {
             rb.velocity = force;
         //gameObject.GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
@@ -107,17 +151,27 @@ public class Lizard : MonoBehaviour
 
     void Flip()
     {
-        facingRight = !facingRight;
-        transform.Rotate(0f, 180f, 0f);
+        if (!isDead) { 
+            facingRight = !facingRight;
+            transform.Rotate(0f, 180f, 0f);
+        }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void Die()
     {
-        if (collision.collider.tag == "Water")
-        {
-            Destroy(gameObject);
+        ChangeAnimation(PLAYER_IDLE);
+        isDead = true;
+        moveSpeed = 0;
+        //go.SetActive(false);
+    }
+
+    void Respawn()
+    {
+        if (isDead) { 
+            isDead = false;
+            //transform.position = new Vector3(checkpoint.position.x, checkpoint.position.y);
+            moveSpeed = 5f;
+            transform.position = new Vector3(checkpoint.position.x, checkpoint.position.y);
         }
-
-
     }
 }
