e51b11e13abf7f31a8f89c6e42d3bc2c191262ca
DIsakov <55271574+DIsakov14504@users.noreply.github.com>
2021-05-12 21:39:54 +0300

    Менюшка епта

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 public class Buttons : MonoBehaviour
 {
     bool flag;
+    public GameObject pauseMenu;
     public void Start()
     {
         flag = false;
@@ -28,18 +29,37 @@ public class Buttons : MonoBehaviour
     // some funcs to see player's profile, to share the game, to like the game and look for information about the game. Until our game appears on the Steam there will be links to our team members' VK profiles
     public void User()
     {
-        Application.OpenURL("https://vk.com/mgoryachev1");
+
     }
     public void Share()
     {
-        Application.OpenURL("https://vk.com/amphibian_kuro");
+
     }
     public void Star()
     {
         Application.OpenURL("https://vk.com/m4ksikk");
+        Application.OpenURL("https://vk.com/dsisakov");
+        Application.OpenURL("https://vk.com/mgoryachev1");
+        Application.OpenURL("https://vk.com/amphibian_kuro");
+
     }
     public void Info()
     {
-        Application.OpenURL("https://vk.com/dsisakov");
+        
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void Exit()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(1);
     }
 }
